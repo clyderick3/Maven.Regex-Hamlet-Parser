0f2391f94e397795dbb4b27c6582242185d00980
Clyde Broderick <clyderick3@gmail.com>
2022-04-15 08:53:48 -0400

    Worked on replacing methods for both Leon and Tariq.

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,13 +1,15 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
  */
 public class HamletParser {
 
-    private String hamletData;
+    private static String hamletData;
     static String originalHamlet = "[H, h][A, a][M, m][L, l][E, e][T, t]";
     static String originalHoratio = "[H,h][O, o][R, r][A, a][T, t][I, i][O, o]";
     static String leon = "Leon";
@@ -37,6 +39,20 @@ public class HamletParser {
     }
 
     public String getHamletData(){
+        return hamletData;
+    }
+
+    public static String replaceHamlet(){
+        Pattern pattern = Pattern.compile(originalHamlet);
+        Matcher matcher = pattern.matcher(hamletData);
+        hamletData = matcher.replaceAll(leon);
+        return hamletData;
+    }
+
+    public static String replaceHoratio(){
+        Pattern pattern = Pattern.compile(originalHoratio);
+        Matcher matcher = pattern.matcher(hamletData);
+        hamletData = matcher.replaceAll(tariq);
         return hamletData;
     }
 
